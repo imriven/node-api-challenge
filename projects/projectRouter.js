81346657693644b740a3136bd400803e69e040fc
Christina Harris <christinathedesigner@outlook.com>
2020-05-08 10:03:50 -0700

    projectRouter complete

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -5,7 +5,11 @@ const actionsdb = require("../actions/actionModel")
 
 
 
-//router.post()
+router.post("/", (req, res) => {
+    db.insert(req.body)
+    .then(result => res.status(201).send())
+    .catch(err => res.status(500).json({message: "error connecting to database"}))
+})
 
 router.get("/", (req, res) => {
     db.get()
@@ -13,20 +17,47 @@ router.get("/", (req, res) => {
     .catch(err => res.status(500).json({message: "error connecting to database"}))
 })
 
-router.get("/:id", (req, res) => {
+router.get("/:id", validateProjectId, (req, res) => {
+   res.status(200).json(req.project)
+})
+
+router.get("/:id/actions", validateProjectId, (req, res) => {
+    db.getProjectActions(req.params.id)
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json({message: "error connecting to database"}))
+ })
+
+ router.put("/:id", validateProjectId, (req, res) => {
+    db.update(req.params.id, req.body)
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json({message: "error connecting to database"}))
+})
+
+ router.delete("/:id", validateProjectId, (req, res) => {
+    db.remove(req.params.id) 
+    .then(result => result === 1 ? res.status(204).send() : res.status(500).json({message: "cannot delete project"}))
+    .catch(err => res.status(500).json({message: "error connecting to database"}))
+})
+
+
+function validateProjectId(req, res, next) {
     db.get(req.params.id)
     .then(result => {
         if (result.length === 0){
             res.status(404).json({message: "project not in database"})
         } else {
-          res.status(200).json(result)  
+            req.project = result
+            next()
         } 
     })
     .catch(err => res.status(500).json({message: "error connecting to database"}))
-})
+}
 
-// router.put()
 
-// router.delete()
+
+
+
+
 
 module.exports = router
+
